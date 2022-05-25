3577143b90fb95c872394e30f6976ada7588f597
Alex_K <a.kirginov@gmail.com>
2022-05-25 20:13:13 +0300

    Add News Portal

---
+++ News Portal/NewsPaper/news/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating_user = models.IntegerField(default=0)
+
+    def update_rating(self):
+        post_rat = self.post_set.aggregate(post_rating=Sum('rating'))
+        p_rat = 0
+        p_rat += post_rat.get('post_rating')
+
+        comment_rat = self.user.comment_set.aggregate(comment_rating=Sum('rating'))
+        c_rat = 0
+        c_rat += comment_rat.get('comment_rating')
+
+        self.rating_user = p_rat * 3 + c_rat
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    ARTICLE = 'AR'
+    NEWS = 'NW'
+    CATEGORY_CHOISES = (
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья'),
+    )
+    article_or_news = models.CharField(max_length=2, choices=CATEGORY_CHOISES, default=ARTICLE)
+
+    time_in = models.DateTimeField(auto_now_add=True)
+    post_category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=64)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def preview(self):
+        return self.text[0:124] + "..."
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+
+
+class Comment(models.Model):
+    comment_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    time_in = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
